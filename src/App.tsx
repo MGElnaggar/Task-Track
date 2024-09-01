14094c23ce83c68e5c84a5ec78b2ef9967a5205d
MGElnaggar <Mostafagamal5827@gmail.com>
2024-09-01 17:52:08 +0300

    fourth+2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={ <TodoList /> } />
           <Route path="/registration" element={ <Registration /> } />
-          <Route path="/postList" element={ <PostsList /> } />
+          {/* <Route path="/postList" element={ <PostsList /> } /> */}
         </Routes>
       </BrowserRouter>
     </>
