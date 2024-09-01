564a9d60d0200fa5326ac0429e5bac5554070ca6
MGElnaggar <Mostafagamal5827@gmail.com>
2024-09-01 17:26:29 +0300

    third commit

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
